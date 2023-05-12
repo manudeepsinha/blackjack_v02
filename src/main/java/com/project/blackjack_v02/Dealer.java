cc13f2b15ccb0de581323bf4b1bac462a3d75554
Nishkarsh Sharma <nishkarsh.sharma1995@gmail.com>
2023-05-12 17:51:01 +0530

    Revert "First hand logic and few other tweaks"

--- src/main/java/com/project/blackjack_v02/Dealer.java
+++ src/main/java/com/project/blackjack_v02/Dealer.java
@@ -4,28 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dealer {
-<<<<<<< HEAD
-
-    private int score = 0;
-    private List<Cards> cardsList = new ArrayList<Cards>();
-
-    public int getScore() {
-        int total = 0;
-        for(Cards card: cardsList) {
-            total += card.getRank().getValue();
-        }
-        return total;
-    }
-    public void setScore(int score) {
-        this.score = score;
-    }
-    public List<Cards> getCardsList() {
-        return cardsList;
-    }
-    public void setCardsList(Cards card) {
-        this.cardsList.add(card);
-    }
-=======
 	
 	private int score = 0;
 	private List<Cards> cardsList = new ArrayList<Cards>();
@@ -42,6 +20,5 @@ public class Dealer {
 	public void setCardsList(List<Cards> cardsList) {
 		this.cardsList = cardsList;
 	}
->>>>>>> 7439b37ff1f99af662b9728b36123d723877414d
 
 }
--- src/main/java/com/project/blackjack_v02/Game.java
+++ src/main/java/com/project/blackjack_v02/Game.java
@@ -1,95 +1,12 @@
 package com.project.blackjack_v02;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Game
 {
-	Deck deck = new Deck();
-	Player sam = new Player();
-	Dealer dealer = new Dealer();
-//	private List<Cards> currCard = new ArrayList<Cards>();
-	private final int BLACKJACK_VALUE = 21;
+	public static void main(String args[]) {
+		Game();
+	}
 
-	public Game() {
+	private static void Game() {
 		// TODO Auto-generated method stub
-
-	}
-
-	public void main()
-	{
-		deck.shuffle();
-//		System.out.println(deck);
-		dealCardsToPlayers();
-
-
-	}
-	private void dealCardsToPlayers()
-	{
-		givePlayerACard();
-		giveDealerACard();
-		givePlayerACard();
-		giveDealerACard();
-
-		System.out.println("Welcome to Blackjack");
-		System.out.println("Sam Cards" + sam.getCardsList());
-		System.out.println("Dealer has " + dealer.getCardsList());
-
-		//getting score
-		System.out.println(sam.getScore());
-		System.out.println(dealer.getScore());
-	}
-
-	private void givePlayerACard()
-	{
-		sam.setCardsList(deck.dealCard());
-	}
-
-	private void giveDealerACard()
-	{
-		dealer.setCardsList(deck.dealCard());
-	}
-
-	private void gameLogic()
-	{
-
-//		check if either player has Blackjack with their initial hand and wins the game.
-//		Blackjack is an initial score of 21 with two cards: A + [10, J, Q, K]
-//		sam wins when both players starts with Blackjack
-
-		if (sam.getScore() == BLACKJACK_VALUE )
-		{
-			System.out.println("Sam wins this round, Congratulations");
-		}
-
-		else if(dealer.getScore() == BLACKJACK_VALUE)
-		{
-			System.out.println("Dealer wins this round");
-		}
-//		dealer wins when both players starts with 22 (A + A)
-//		sam has lost the game if their total is higher than 21
-
-		else if (sam.getScore() > BLACKJACK_VALUE)
-		{
-			System.out.println("Sam has lost, Dealer Wins");
-		}
-
-		//the dealer has lost the game if their total is higher than 21
-		else if(dealer.getScore() > BLACKJACK_VALUE)
-		{
-			System.out.println("Sam wins this round, Congratulations");
-		}
-		else
-		{
-			//sam can start drawing cards
-			while(sam.getScore() < 17)
-			{
-				//sam will draw cards
-			}
-
-		}
-
-
-
+		
 	}
 }
